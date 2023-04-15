0f32ef1f9566bc97fdb37ef50c2a05549d490ef0
Agustin Szyszczyc <szyagustin@laika.com>
2023-04-14 22:22:14 -0300

    dark mode changes

--- src/components/DarkMode.tsx
+++ src/components/DarkMode.tsx
@@ -3,20 +3,39 @@ import Switch from 'react-switch';
 import { AppContext } from '../context/AppContext';
 import { FlexCenterBox } from './FlexCenterBox';
 import styled from 'styled-components';
+import { BsMoonFill } from 'react-icons/bs';
+import { BsFillSunFill } from 'react-icons/bs';
 
 const DarkMode = () => {
   const { darkMode, setDarkMode } = useContext(AppContext);
   return (
     <DarkModeBox>
-      <p>Dark mode</p>
       <SwitchBox>
         <Switch
           onChange={() => setDarkMode(!darkMode)}
           checked={darkMode}
-          onColor='#00d75d'
+          onColor='#341ff0'
+          offColor='#02d5ff'
+          checkedIcon={<DarkModeOnIcon />}
+          uncheckedIcon={<DarkModeOffIcon />}
         />
       </SwitchBox>
     </DarkModeBox>
+  );
+};
+
+const DarkModeOnIcon = () => {
+  return (
+    <IconBox>
+      <BsMoonFill />
+    </IconBox>
+  );
+};
+const DarkModeOffIcon = () => {
+  return (
+    <IconBox>
+      <BsFillSunFill color='#ffffff' />
+    </IconBox>
   );
 };
 
@@ -32,4 +51,12 @@ const SwitchBox = styled.div`
   margin-left: 5px;
   margin-bottom: 0;
   padding: 0;
+`;
+
+const IconBox = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 28px;
+  width: 30px;
 `;
